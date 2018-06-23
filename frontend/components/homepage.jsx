8b5a8f1fdb9ba313cabafc602cc74565ab67f230
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2018-06-22 21:55:36 -0700

    home page component complete minus fetch

--- frontend/components/homepage.jsx
+++ frontend/components/homepage.jsx
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 //import '../style/nav.scss';
 
 class Homepage extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      url:'',
+    }
+    this.handleChange=this.handleChange.bind(this)
+    this.handleSubmit=this.handleSubmit.bind(this)
+  }
+  
+  handleChange(ev) {
+    this.setState({url:ev.target.value})
+  }
+
+  handleSubmit(ev) {
+    ev.preventDefault()
+    console.log(this.state)
+  }
+  
   render() {
     return (<div className="homepage">
       <h2>Tag-It</h2>
@@ -9,6 +27,10 @@ class Homepage extends React.Component {
         <h1> About this App </h1>
         <p> Have tons of images and need a way to organize them, Tag-It can help </p>
       </div>
+      <form onSubmit={this.handleSubmit}>
+      <input name="url" type="text" onChange={this.handleChange} value={this.state.url}/>
+      <button type="submit">Submit</button>
+      </form>
     </div>
     );
   }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 // import {createStore} from 'redux';
 // import {Provider} from 'react-redux';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 import ReactDOM from 'react-dom'
 //import '../style/reset.scss'
 
 import FileTag from '../components/file-tag.jsx';
-import Navbar from '../components/nav.jsx';
 import Aboutus from '../components/about-us.jsx';
 //import SearchPage from '../components/searchpage.jsx';
 import HomePage from '../components/homepage.jsx'
@@ -19,14 +18,23 @@ class App extends React.Component {
     render() {
       return <BrowserRouter>
         <div>
-          <Navbar />
+          <nav>
+          <ul>
+          <li><Link to="/">Home Page</Link></li>
+          <li><Link to="/file-tag">File Tag</Link></li>
+          <li><Link to="/">Search</Link></li>
+          <li><Link to="/about-us">About Us</Link></li>
+          </ul>
+          </nav>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/file-tag" component={FileTag} />
-          <Route exact path="/search" component={SearchPage} />
+          {/*<Route exact path="/search" component={SearchPage} />*/}
           <Route exact path="/about-us" component={Aboutus} />
         </div>
       </BrowserRouter>
     }
   }
   
-export default App
+  let root = document.createElement('div')
+  document.body.appendChild(root)
+  ReactDOM.render(<App/>, root)
